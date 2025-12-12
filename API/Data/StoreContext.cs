953a51d38225e0f8a57ed5a9841c0c901b41bfd5
Denis Wonder <denis.chudnov.97@gmail.com>
2025-12-12 23:23:23 +0100

    Creating Entity framework migration

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -4,12 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class StoreContext: DbContext
+public class StoreContext : DbContext
 {
-    public StoreContext(DbContextOptions options)
+    public StoreContext(DbContextOptions options): base(options)
     {
         
     }
-
-    public DbSet<Product> Products { get; set; }
+    public required DbSet<Product> Products { get; set; }
 }
