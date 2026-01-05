ead5fd76b46ec35954930e89a473ccdf36ff7046
Denis Wonder <denis.chudnov.97@gmail.com>
2026-01-05 16:15:39 +0100

    Fetching an individual product from the API

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,19 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import type {Product} from "../../app/models/product.ts";
+
 export default function ProductDetails() {
+    const {id} = useParams();
+    const [product, setProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        fetch(`https://localhost:5001/api/products/${id}`)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+            .catch(err => console.log(err));
+    }, [id]);
+    
     return (
-        <div>Product Details page</div>
+        <div>The name of product is: {product?.name}</div>
     )
 }
