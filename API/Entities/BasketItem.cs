e22845ab8fe79ffc136b92973ed0f63aa58c38f1
Denis Chudnov <denis.chudnov.97@gmail.com>
2026-01-12 17:00:39 +0100

    Update db schema for working with Basket and Basket Item entities

--- API/Entities/BasketItem.cs
+++ API/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
-﻿namespace API.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
+namespace API.Entities;
+
+[Table("BasketItems")]
 public class BasketItem
 {
     public int Id { get; set; }
@@ -7,4 +10,7 @@ public class BasketItem
     //navigation properties
     public int ProductId { get; set; }
     public required Product Product { get; set; }
+    
+    public int BasketId { get; set; }
+    public Basket Basket { get; set; } = null!;
 }
