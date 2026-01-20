bf46691612f7d7bd4285067c6581918c85143f38
Denis Chudnov <denis.chudnov.97@gmail.com>
2026-01-20 17:59:50 +0100

    Start working on adding of pagination to the API. Added helper classes with list, metadata and params.

--- API/RequestHelpers/ProductParams.cs
+++ API/RequestHelpers/ProductParams.cs
@@ -1,6 +1,6 @@
 ﻿namespace API.RequestHelpers;
 
-public class ProductParams
+public class ProductParams: PaginationParams
 {
     public string? OrderBy { get; set; }
     public string? SearchTerm { get; set; }
