d82569abb12bad95eb9fdc3e89bdb0d569b29689
Denis Wonder <denis.chudnov.97@gmail.com>
2026-01-26 16:15:11 +0100

    Adding filters functionality to client catalog page

--- client/src/features/catalog/Filters.tsx
+++ client/src/features/catalog/Filters.tsx
@@ -1,6 +1,10 @@
-import {useFetchFiltersQuery} from "./catalogApi.ts";
-import {Box, Checkbox, FormControl, FormControlLabel, FormGroup, Paper, Radio} from "@mui/material";
+import {Box, Button, Paper, Typography} from "@mui/material";
 import Search from "./Search.tsx";
+import RadioButtonGroup from "../../app/shared/components/RadioButtonGroup.tsx";
+import {useAppDispatch, useAppSelector} from "../../app/store/store.ts";
+import {resetParams, setBrands, setOrderBy, setTypes} from "./catalogSlice.ts";
+import CheckboxButtons from "../../app/shared/components/CheckboxButtons.tsx";
+import {useFetchFiltersQuery} from "./catalogApi.ts";
 
 const sortOptions = [
     {value: 'name', label: 'Alphabetical'},
@@ -9,48 +13,40 @@ const sortOptions = [
     {value: 'priceDesc', label: 'Price: High to low'}
 ]
 
-export default function Filters(){
+export default function Filters() {
     const {data} = useFetchFiltersQuery();
+    const { orderBy, types, brands } = useAppSelector(state => state.catalog);
+    const dispatch = useAppDispatch();
+
+    if (!data?.brands || !data?.types) return <Typography>Loading...</Typography>
     
     return (
-        <Box display="flex" flexDirection="column" gap={3}>
+        <Box display='flex' flexDirection='column' gap={3}>
             <Paper>
                 <Search />
             </Paper>
-            <Paper sx={{p:3}}>
-                <FormControl>
-                    {sortOptions.map(({value, label}) => (
-                        <FormControlLabel
-                            key={label}
-                            control={<Radio sx={{py: 0.7}} />}
-                            label={label}
-                            value={value}
-                        />
-                    ))}
-                </FormControl>
+            <Paper sx={{ p: 3 }}>
+                <RadioButtonGroup
+                    selectedValue={orderBy}
+                    options={sortOptions}
+                    onChange={e => dispatch(setOrderBy(e.target.value))}
+                />
             </Paper>
-            <Paper sx={{p:3}}>
-                <FormGroup>
-                    {data && data.brands.map(item => (
-                        <FormControlLabel
-                            key={item}
-                            control={<Checkbox color='secondary' sx={{py:0.7, fontSize: 40}} />}
-                            label={item}
-                        />
-                    ))}
-                </FormGroup>
+            <Paper sx={{ p: 3 }}>
+                <CheckboxButtons
+                    items={data.brands}
+                    checked={brands}
+                    onChange={(items: string[]) => dispatch(setBrands(items))}
+                />
             </Paper>
-            <Paper sx={{p:3}}>
-                <FormGroup>
-                    {data && data.types.map(item => (
-                        <FormControlLabel
-                            key={item}
-                            control={<Checkbox color='secondary' sx={{py:0.7, fontSize: 40}} />}
-                            label={item}
-                        />
-                    ))}
-                </FormGroup>
+            <Paper sx={{ p: 3 }}>
+                <CheckboxButtons
+                    items={data.types}
+                    checked={types}
+                    onChange={(items: string[]) => dispatch(setTypes(items))}
+                />
             </Paper>
+            <Button onClick={() => dispatch(resetParams())}>Reset filters</Button>
         </Box>
     )
 }
