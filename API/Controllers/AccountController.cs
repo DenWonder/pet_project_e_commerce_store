517853afcc7ad4240ec15c99d05daa51fceed60e
Denis Wonder <denis.chudnov.97@gmail.com>
2026-01-28 16:25:53 +0100

    Add a couple of additional account api endpoints; add user address functionality.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
 using API.DTOs;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -27,5 +29,60 @@ public class AccountController(SignInManager<User> signInManager): BaseApiContro
         await signInManager.UserManager.AddToRoleAsync(user, "Member");
 
         return Ok();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity.IsAuthenticated == false)
+        {
+            return NoContent();
+        }
+
+        var user = await signInManager.UserManager.GetUserAsync(User);
+        if (user == null) return Unauthorized();
+        var roles = await signInManager.UserManager.GetRolesAsync(user);
+        return Ok(new
+        {
+            user.Email,
+            user.UserName,
+            Roles = roles
+        });
+    }
+
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return NoContent();
+    }
+
+    [Authorize]
+    [HttpPost("address")]
+    public async Task<ActionResult<Address>> CreateOrUpdateAddress(Address address)
+    {
+        var user = await signInManager.UserManager.Users
+            .Include(x => x.Address)
+            .FirstOrDefaultAsync(x => x.UserName == User.Identity!.Name);
+
+        if (user == null) return Unauthorized();
+        user.Address = address;
+        var result = await signInManager.UserManager.UpdateAsync(user);
+        if (!result.Succeeded) return BadRequest("Problem updating user address");
+        return Ok(user.Address);
+    }
+
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<ActionResult<Address>> GetSavedAddress()
+    {
+        var address = await signInManager.UserManager.Users
+            .Where(x => x.UserName == User.Identity!.Name)
+            .Select(x => x.Address)
+            .FirstOrDefaultAsync();
+
+        if (address == null) return NoContent();
+
+        return address;
     }
 }
