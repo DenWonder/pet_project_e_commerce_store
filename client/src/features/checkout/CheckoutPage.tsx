3396e7897d1b0ae81d6209e9504eecdda2b7b0b2
Denis Wonder <denis.chudnov.97@gmail.com>
2026-01-30 17:56:18 +0100

    Setting up stripe on the client

--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -1,12 +1,34 @@
-import {Grid2} from "@mui/material";
+import {Grid2, Typography} from "@mui/material";
 import OrderSummary from "../../app/shared/components/OrderSummary";
 import CheckoutStepper from "./CheckoutStepper.tsx";
+import {loadStripe, type StripeElementsOptions} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
+import {useFetchBasketQuery} from "../basket/basketApi.ts";
+import {useMemo} from "react";
+
+const stripePromise = loadStripe(import.meta.env.STRIPE_PK);
 
 export default function CheckoutPage() {
+    const {data: basket} = useFetchBasketQuery();
+    const options: StripeElementsOptions | undefined = useMemo(() => {
+        if (!basket?.clientSecret) return undefined;
+        return {
+            clientSecret: basket.clientSecret
+        }
+    }, [basket?.clientSecret])
+    
     return (
         <Grid2 container spacing={2}>
             <Grid2 size={8}>
-                <CheckoutStepper />
+                {!stripePromise || !options ? (
+                    <Typography variant="h5" component="div">
+                        Loading checkout ...      
+                    </Typography>
+                ) : (
+                    <Elements stripe={stripePromise} options={options}>
+                        <CheckoutStepper />
+                    </Elements>
+                )}
             </Grid2>
             <Grid2 size={4}>
                 <OrderSummary />
