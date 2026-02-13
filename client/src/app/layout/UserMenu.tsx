b0da4daf2161cabba856e41f9dcacd89c6d7e11b
Denis Chudnov <denis.chudnov.97@gmail.com>
2026-02-13 14:08:49 +0100

    Create inventory page on the client app

--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -1,7 +1,7 @@
 import { Button, Menu, Fade, MenuItem, ListItemIcon, ListItemText, Divider } from "@mui/material";
 import { useState } from "react";
 import type {User} from "../models/user.ts";
-import { History, Logout, Person } from "@mui/icons-material";
+import {History, Inventory, Logout, Person} from "@mui/icons-material";
 import { useLogoutMutation } from "../../features/account/accountApi";
 import {Link} from "react-router-dom";
 
@@ -51,6 +51,12 @@ export default function UserMenu({ user }: Props) {
                         <History />
                     </ListItemIcon>
                     <ListItemText>My orders</ListItemText>
+                </MenuItem>
+                <MenuItem component={Link} to={'/inventory'}>
+                    <ListItemIcon>
+                        <Inventory />
+                    </ListItemIcon>
+                    <ListItemText>Inventory</ListItemText>
                 </MenuItem>
                 <Divider />
                 <MenuItem onClick={logout}>
